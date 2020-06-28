0163fe443735fa01c0b1dceaaa3c38405c04646d
Anna Munhoz <annamunhoz@tokenlab.com.br>
2020-06-28 10:32:19 -0300

    update themeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:royalgameofur/app_provider.dart';
+import 'package:royalgameofur/presentation/common/app_colors.dart';
 import 'package:royalgameofur/presentation/scenes/game/game_bloc.dart';
 import 'package:royalgameofur/presentation/scenes/game/game_page.dart';
 import 'package:royalgameofur/presentation/home_page.dart';
@@ -18,6 +19,39 @@ class MyApp extends StatelessWidget {
         title: 'Royal Game of Ur',
         theme: ThemeData(
           primarySwatch: Colors.purple,
+          backgroundColor: AppColors.backgroundGameColor,
+          appBarTheme: AppBarTheme(
+              color: AppColors.backgroundGameColor,
+              textTheme: TextTheme(
+                headline6: TextStyle(
+                  fontFamily: 'VCR_OSD',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              )),
+          textTheme: TextTheme(
+              bodyText2: TextStyle(
+                fontFamily: 'VCR_OSD',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                letterSpacing: 1.5,
+              ),
+              headline6: TextStyle(
+                fontFamily: 'VCR_OSD',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                letterSpacing: 1.5,
+              ),
+              button: TextStyle(
+                decoration: TextDecoration.underline,
+                fontFamily: 'VCR_OSD',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              )),
         ),
         home: HomePage(),
         routes: {
