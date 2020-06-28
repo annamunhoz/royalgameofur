54c54d441ddf14d7d10f96e810ef4f377a361364
Vinicius Munhoz <viniciusmunhoz@live.com>
2020-06-28 15:21:23 -0300

    finished tutorial

--- lib/presentation/scenes/game_rules/game_rules_page.dart
+++ lib/presentation/scenes/game_rules/game_rules_page.dart
@@ -1,7 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:royalgameofur/presentation/common/app_colors.dart';
 
 class GameRulesPage extends StatefulWidget {
@@ -16,29 +15,52 @@ class _GameRulesPageState extends State<GameRulesPage> {
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
+    final height = MediaQuery.of(context).size.height;
 
-    return CarouselSlider(
-      items: imageSliders,
-      options: CarouselOptions(
-          height: height * 0.9,
-          viewportFraction: 1.0,
-          enlargeCenterPage: false,
-          aspectRatio: 2.0,
-          onPageChanged: (index, reason) {
-            setState(() {
-              _current = index;
-            });
-          }),
+    return Column(
+      children: <Widget>[
+        Spacer(),
+        CarouselSlider(
+          items: imageSliders,
+          options: CarouselOptions(
+              height: height * 0.9,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              aspectRatio: 2.0,
+              enableInfiniteScroll: false,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              }),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: imgList.map((image) {
+            int index = imgList.indexOf(image);
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _current == index
+                    ? Color.fromRGBO(246, 0, 255, 0.9)
+                    : Color.fromRGBO(246, 0, 255, 0.4),
+              ),
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 
   // TODO: update images
   static final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'assets/images/tutorial1.png',
+    'assets/images/tutorial2.png',
+    'assets/images/tutorial3.png',
+    'assets/images/tutorial4.png',
   ];
 
   final List<Widget> imageSliders = imgList
@@ -46,24 +68,12 @@ class _GameRulesPageState extends State<GameRulesPage> {
         (item) => Material(
           color: AppColors.backgroundGameColor,
           child: Container(
-            color: AppColors.playerOneColor,
-            margin: const EdgeInsets.all(5),
+            color: AppColors.backgroundGameColor,
             child: Stack(children: <Widget>[
-              Image.network(item, fit: BoxFit.cover, width: 1000),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 20,
-                  ),
-                  child: Text(
-                    '${imgList.indexOf(item) + 1}',
-                    style: const TextStyle(fontSize: 20),
-                  ),
-                ),
+              Image.asset(
+                item,
+                fit: BoxFit.contain,
+                width: 1000,
               ),
             ]),
           ),
